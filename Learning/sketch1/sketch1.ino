4bf9c2bd28899810cc6104ede4ef891dfcd0b90a
TarushGupta23 <tarush23.gupta@gmail.com>
2024-01-12 17:20:12 +0530

    initial commit

---
+++ Learning/sketch1/sketch1.ino
@@ -0,0 +1,21 @@
+int ledPin = 13;
+String msg = "This is message: ";
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  Serial.begain(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i=0; i<3; i++) {
+    Serial.print(msg);
+    Serial.println(i);
+  }
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+
+  Serial.println("Hello world");
+}
